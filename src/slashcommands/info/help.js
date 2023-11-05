abcdf697691705fcf1d05a40bd98e7225c0c0a61
Nevtr4l <zlarosavalladares@gmail.com>
2023-11-05 05:52:12 -0500

    Comando help finalizado

--- src/slashcommands/info/help.js
+++ src/slashcommands/info/help.js
@@ -12,18 +12,20 @@ module.exports = {
         COMANDO_OPTION = interaction.options.getString("comando")
 
         if (COMANDO_OPTION) {
-            SLASH = CLIENT.slash.get(COMANDO_OPTION.toLowerCase())
-
+            SLASH = SLASH_LISTA.find(c => c.name == COMANDO_OPTION)
+            COMANDO_PREFIJO = CLIENT.comandos.get(COMANDO_OPTION.toLowerCase()) || CLIENT.comandos.find(c => c.aliases && c.aliases.includes(COMANDO_OPTION.toLowerCase()))
+            ALIASES = (COMANDO_PREFIJO && COMANDO_PREFIJO.aliases && COMANDO_PREFIJO.aliases.length >= 1) ? COMANDO_PREFIJO.aliases.slice(0, 5).join(", ") : "No hay otros nombres para este comando."
             if (SLASH) {
-                NAME = SLASH.CMD.name
-                NAME_FIRST_MAYUS = NAME.charAt(0).toUpperCase() + NAME.slice(1).toLowerCase()
-                DESCRIPTION = SLASH.CMD.description
-                EMBED = new EmbedBuilder().setColor(process.env.COLOR).setDescription(`# <:star:1143684933399887942> ${NAME_FIRST_MAYUS}\nDescripción: ${DESCRIPTION}`)
+                EMBED = new EmbedBuilder().setColor(process.env.COLOR).setFooter({ text: `© ${CLIENT.user.username} | 2023 - Presente`, iconURL: CLIENT.user.displayAvatarURL({ dynamic: true }) })
+                .setDescription(`# ${SLASH.description.split(" | ")[0].split(" ")[0]} Comando </${SLASH.name}:${SLASH.id}>`)
+                .addFields(
+                    { name: "📚 Categoría", value: `\`\`\`ansi\n${color(SLASH.description.split(" | ")[0].split(" ")[1], "verde")}\`\`\``, inline: true},
+                    { name: "🎫 Otros nombres", value: `\`\`\`ansi\n${color(ALIASES, "verde")}\`\`\``, inline: true},
+                    { name: "📦 Descripción de uso del comando", value: `\`\`\`ansi\n${color(SLASH.description.split(" | ")[1], "verde")}\`\`\``, inline: false}
+                )
                 interaction.reply({ embeds: [EMBED] })
-
             } else {
-                EMBED = new EmbedBuilder().setColor(process.env.COLOR).setDescription(`# <:skull:1143684711944814672> Error en el comando\nEl comando **${COMANDO_OPTION}** no existe. Prueba a usar el comando "/help" sin ningún parámetro para obtener todos los comandos existentes.`)
-                interaction.reply({ embeds: [EMBED] })
+                interaction.reply({ content: "El comando especificado no existe.", ephemeral: true })
             }
             
         } else if (!COMANDO_OPTION) {
@@ -35,7 +37,7 @@ module.exports = {
                 {name: `▾ 📊 **Comandos prefijo \`[ ${process.env.PREFIJO} ]\`**`, value: `\`\`\`ansi\n${color(`- ${CLIENT.comandos.size} comandos`, "verde")}\`\`\``, inline: true},
                 {name: "▾ 🧨 **Comandos slash \`[ / ]\`**", value: `\`\`\`ansi\n${color(`- ${SLASH_LISTA.size} slashcommands`, "verde")}\`\`\``, inline: true},
                 {name: "▾ 🎫 **Ayuda de comandos**", value: `También puedes aprender más acerca de un comando escribiendo:\`\`\`ansi\n${color(`/help [comando] | >>> Ejemplo: /help ${SLASH_LISTA.toJSON()[Math.floor(Math.random() * (SLASH_LISTA.size-1))].name} <<<`, "verde")}\`\`\``, inline: false},
-                {name: "▾ 📦 **Más información**", value: `[Servidor de Soporte](${process.env.SOPORTE}) | [GitHub (Open Source)](${process.env.REPOSITORIO}) | [Invítame a tu servidor](${process.env.INVITE})`, inline: false},
+                {name: "▾ 📦 **Más información**", value: `[Servidor de Soporte](${process.env.SOPORTE}) | [GitHub (Open Source)](${process.env.REPOSITORIO}) | [Invítame a tu servidor](${process.env.INVITE})`, inline: false}
             )
             SELECCION = new ActionRowBuilder().addComponents(new StringSelectMenuBuilder()
             .setCustomId(`SelecciónMenuAyuda`).setPlaceholder("Elige una categoría para ver sus comandos")
@@ -48,8 +50,7 @@ module.exports = {
                     emoji: categ(categoria).split(" ")[0],
                 }
                 return OBJETO
-            }))
-        )
+            })))
             interaction.reply({ embeds: [EMBED], components: [SELECCION] })
         }
     }
